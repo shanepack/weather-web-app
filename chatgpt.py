729ec59ab0f529afbce0eef02049144df8a0e5d0
Rooted1 <ruthobey@gmail.com>
2023-10-21 00:13:31 -0500

    set system role as a weather forecaster

--- chatgpt.py
+++ chatgpt.py
@@ -8,15 +8,16 @@ def load_api_key(secrets_file="secrets.json"):
 
 api_key = load_api_key()
 
-content = input("User: ")
-
 openai.api_key = api_key
 
+content = input("User: ")
+messages = [
+    {"role": "system", "content": "You are a weather forecaster"}
+    ]
+messages.append({"role": "user", "content": content})
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": content}
-	]
+    messages=messages
 )
 chat_response = completion.choices[0].message.content
 print(f'ChatGPT: {chat_response}')
