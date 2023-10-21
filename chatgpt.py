b505150144f1fe838438fafb62d6c72feb40eb85
Rooted1 <ruthobey@gmail.com>
2023-10-21 00:18:05 -0500

    allow user to enter many questions per interaction

--- chatgpt.py
+++ chatgpt.py
@@ -9,15 +9,15 @@ def load_api_key(secrets_file="secrets.json"):
 api_key = load_api_key()
 
 openai.api_key = api_key
-
-content = input("User: ")
-messages = [
-    {"role": "system", "content": "You are a weather forecaster"}
-    ]
-messages.append({"role": "user", "content": content})
-completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages
-)
-chat_response = completion.choices[0].message.content
-print(f'ChatGPT: {chat_response}')
+while True:
+	content = input("User: ")
+	messages = [
+		{"role": "system", "content": "You are a weather forecaster"}
+		]
+	messages.append({"role": "user", "content": content})
+	completion = openai.ChatCompletion.create(
+		model="gpt-3.5-turbo",
+		messages=messages
+	)
+	chat_response = completion.choices[0].message.content
+	print(f'ChatGPT: {chat_response}')
